e2a0cad410e12fe6163979e70f3488042a91eecb
shellykuo <shelly.kuo@hearts.tw>
2021-12-28 18:01:09 +0800

    add buy function in PageNftHome

--- consumer-site/src/pages/page-nft-home/index.js
+++ consumer-site/src/pages/page-nft-home/index.js
@@ -7,7 +7,9 @@ import { Box, Button, Card, CardActions, CardContent, CardHeader, CardMedia, Con
 import Web3 from 'web3';
 import NftMarket from '../../contracts/NftMarket.json';
 import NftAmazons from '../../contracts/NftAmazons.json';
+import MainCoin from '../../contracts/MainCoin.json';
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import { useWallet } from 'use-wallet';
 
 const cardWidth = (1152 - 4 * 8 * 3) / 3
 
@@ -18,7 +20,7 @@ const cardWidth = (1152 - 4 * 8 * 3) / 3
 //   stamp.
 //   meta: { name, description, image }
 // }
-const NftCard = ({book}) => 
+const NftCard = ({book, onBuy: onPurchase}) => 
   <Card sx={{m:2, width: cardWidth}}>
     <CardMedia 
       component="img" 
@@ -36,16 +38,32 @@ const NftCard = ({book}) =>
       }
     </CardContent>
     <CardActions sx={{display: 'flex', justifyContent: 'space-between'}}>
-      <Button>Buy now</Button>
+      <Button onClick={onPurchase}>Buy now</Button>
       <IconButton><FavoriteBorderIcon /></IconButton>
     </CardActions>
   </Card>;
 
 export const PageNftHome = () => {
   const [market, dispatchMarket] = useReducer(NftMarketReducer, NftMarketInitial);
+  const wallet = useWallet();
   const {contract} = useContract(NftMarket);
   const {contract:contractAmazons} = useContract(NftAmazons);
-  
+  const {contract:contractCoin} = useContract(MainCoin);
+
+  const onPurchase = async(sku) => {
+    try {
+      if(!wallet.isConnected())
+        throw new Error("Wallet not connected!");
+
+      await contractCoin.methods
+        .apporve(contract.options.address, Web3.utils.toWei(sku.price)).send({from:wallet.account});
+
+      let res = await contract.methods.buy(sku.bookId).send({from:wallet.account});
+    }catch(err){
+      alert(err.message)
+    }
+  }
+
   useEffect(()=>{
     if(!contract) return;
     (async () => {
@@ -74,7 +92,10 @@ export const PageNftHome = () => {
               <NftMarketContext.Consumer>
                 {({market}) =>
                   market.skus && Object.values(market.skus).map(
-                    sku => <NftCard key={`${sku.nftContract}/${sku.tokenId}`} book={sku} />
+                    sku => <NftCard
+                     key={`${sku.nftContract}/${sku.tokenId}`}
+                     book={sku} 
+                     onPurchase={()=>onPurchase(sku)}/>
                   )
                 }
               </NftMarketContext.Consumer>
